3f99ded1cf316c737154095f9293721ed64a9e64
jimleitch01 <jim.leitch@sunningdale.nl>
2014-03-20 22:03:53 +0000

    onetwothree

--- instance-o-mizer.bash
+++ instance-o-mizer.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #  instance-o-mizer.bash
-#  
+#  deleetme
 #
 #  Created by Jim Leitch on 3/17/14.
 #
@@ -9,7 +9,7 @@
 . ~/keystonerc_admin
 COLOR=$1
 SERVERTYPE=$2
-
+ 
 
 OUTPUT=`nova boot --key-name master  --flavor db2fc608-e6cf-4f59-a397-ba1c5043761d --image af5ff05d-5b31-40d0-b240-0c4b6742c633 ${COLOR}-${SERVERTYPE}`
 
