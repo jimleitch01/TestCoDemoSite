081d09b050d8e36c04aed6825798bfddba9f93e4
jimleitch01 <jim.leitch@sunningdale.nl>
2014-03-21 07:05:08 +0100

    onetwothree

--- instance-o-mizer.bash
+++ instance-o-mizer.bash
@@ -9,10 +9,9 @@ FLAVOR=db2fc608-e6cf-4f59-a397-ba1c5043761d
 IMAGE=af5ff05d-5b31-40d0-b240-0c4b6742c633
 COLOR=$1
 SERVERTYPE=$2
-
+INSTANCENAME=${COLOR}-${SERVERTYPE}
 
 . ~/keystonerc_admin
-
 
 INSTANCEID=`nova boot --key-name master  --flavor  $FLAVOR --image $IMAGE ${COLOR}-${SERVERTYPE} | grep " id " | awk '{print $4}'`
 sleep 3
@@ -20,5 +19,21 @@ INSTANCEFLOATINGIP=`nova show $INSTANCEID | grep novanetwork | awk '{print $6}'`
 
 echo INSTANCEID=$INSTANCEID
 echo INSTANCEFLOATINGIP=$INSTANCEFLOATINGIP
+
+#DNS
+
+grep STATIC /etc/hosts > /etc/hosts
+
+for INSTANCE in `nova list | grep ACTIVE | awk '{print $2}'`;
+do
+
+	DNSNAME=`nova show $INSTANCE | grep name | awk '{print $4}'`
+	DNSIP=`nova show $INSTANCE | grep novanetwork | awk '{print $6}'`
+    echo $DNSIP $DNSNAME >> /etc/hosts
+done
+
+
+
+
 
 
