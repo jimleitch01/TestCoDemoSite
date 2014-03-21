edf5d42fe46bffe0a9be88fcdd1f57737a35d394
jimleitch01 <jim.leitch@sunningdale.nl>
2014-03-21 06:52:34 +0100

    onetwothree

--- instance-o-mizer.bash
+++ instance-o-mizer.bash
@@ -15,7 +15,7 @@ SERVERTYPE=$2
 
 
 INSTANCEID=`nova boot --key-name master  --flavor  $FLAVOR --image $IMAGE ${COLOR}-${SERVERTYPE} | grep " id " | awk '{print $4}'`
-nova show $INSTANCEID | grep novanetwork | awk '{print $6}'
+sleep 3
 INSTANCEFLOATINGIP=`nova show $INSTANCEID | grep novanetwork | awk '{print $6}'`
 
 echo INSTANCEID=$INSTANCEID
