500a6e4615dc148278832c39437a036d747a6393
jimleitch01 <jim.leitch@sunningdale.nl>
2014-03-24 01:05:22 +0100

    onetwothree

--- orchestrate-o-mizer.bash
+++ orchestrate-o-mizer.bash
@@ -5,6 +5,8 @@
 # Run ansible playbooks
 
 sudo ansible-playbook -i /etc/ansible/hosts playbooks/homegrown/testco_init.yaml
+echo sleeping 120 for reboots
+sleep 120
 sudo ansible-playbook -i /etc/ansible/hosts playbooks/examples/lamp_simple/site.yml 
 sudo ansible-playbook -i /etc/ansible/hosts playbooks/examples/jboss-standalone/site.yml 
 
