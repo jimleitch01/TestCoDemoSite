0a4062d0dbf2b7a6326d64664567cd2dff7fcba4
jimleitch01 <jim.leitch@sunningdale.nl>
2014-03-29 08:09:08 +0100

    onetwothree

--- orchestrate-o-mizer.bash
+++ orchestrate-o-mizer.bash
@@ -2,6 +2,31 @@
 # Run ansible playbooks
 COLOR=$1
 
+echo +++Sleeping 30 seconds to ensure server startup
+sleep 30
+# echo +++Wait until all instances operational
+# ALLHOSTSLIVE=0
+# DINK="."
+# while [[ $ALLHOSTSLIVE -eq 0 ]];
+# do
+# ALLHOSTSLIVE=0
+# for HOST in $HOST_LIST;
+#    do
+#    	  echo $DINK
+#    	  DINK=$DINK"."
+#    	  sleep 2
+#    	  /usr/bin/nc -w 1 -z $HOST 22
+#       if [[ $? = "0" ]];
+#       then 
+#       	ALLHOSTSLIVE=1
+#       else
+#       	ALLHOSTSLIVE=0
+#       	break
+#       fi
+#    done
+# done
+# echo +++All instances operational
+
 
 sudo ansible-playbook -i /etc/ansible/hosts playbooks/homegrown/testco_init.yaml
 sudo ansible-playbook -i /etc/ansible/hosts playbooks/examples/lamp_simple/site.yml 
