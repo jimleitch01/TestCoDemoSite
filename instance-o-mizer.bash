e9e1265804b27d7fe88b2071ba3bb10736145696
z792800 <z792800@gmail.com>
2015-01-27 20:50:19 +0100

    Update instance-o-mizer.bash

--- instance-o-mizer.bash
+++ instance-o-mizer.bash
@@ -40,7 +40,7 @@ echo +++Starting Instance ${COLOR}-${SERVERTYPE}
 echo +++"nova boot --key-name denniskp --nic net-id=000f5738-97f5-418c-9331-fcae0b39c9b  --flavor  $FLAVOR --image $IMAGE ${COLOR}-${SERVERTYPE}"
 
 
-INSTANCEID=`nova boot --key-name master  --flavor  $FLAVOR --image $IMAGE ${COLOR}-${SERVERTYPE} | grep " id " | awk '{print $4}'`
+INSTANCEID=`nova boot --key-name denniskp --nic net-id=000f5738-97f5-418c-9331-fcae0b39c9b  --flavor  $FLAVOR --image $IMAGE ${COLOR}-${SERVERTYPE} | grep " id " | awk '{print $4}'`
 
 TIMEOUTCOUNTER=120
 while [[ `nova list | grep ACTIVE | grep $INSTANCEID` = "" ]];
