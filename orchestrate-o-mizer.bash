ec8c695651a162dcfe9a94826bad9443340b28dc
z792800 <z792800@gmail.com>
2015-02-01 15:06:26 +0100

    Update orchestrate-o-mizer.bash

--- orchestrate-o-mizer.bash
+++ orchestrate-o-mizer.bash
@@ -2,30 +2,30 @@
 # Run ansible playbooks
 COLOR=$1
 
-echo +++Sleeping 30 seconds to ensure server startup
-sleep 30
-# echo +++Wait until all instances operational
-# ALLHOSTSLIVE=0
-# DINK="."
-# while [[ $ALLHOSTSLIVE -eq 0 ]];
-# do
-# ALLHOSTSLIVE=0
-# for HOST in $HOST_LIST;
-#    do
-#    	  echo $DINK
-#    	  DINK=$DINK"."
-#    	  sleep 2
-#    	  /usr/bin/nc -w 1 -z $HOST 22
-#       if [[ $? = "0" ]];
-#       then 
-#       	ALLHOSTSLIVE=1
-#       else
-#       	ALLHOSTSLIVE=0
-#       	break
-#       fi
-#    done
-# done
-# echo +++All instances operational
+echo +++Sleeping 50 seconds to ensure server startup
+sleep 50
+ echo +++Wait until all instances operational
+ ALLHOSTSLIVE=0
+ DINK="."
+ while [[ $ALLHOSTSLIVE -eq 0 ]];
+ do
+ ALLHOSTSLIVE=0
+ for HOST in $HOST_LIST;
+    do
+    	  echo $DINK
+    	  DINK=$DINK"."
+    	  sleep 2
+    	  /usr/bin/nc -w 1 -z $HOST 22
+       if [[ $? = "0" ]];
+       then 
+       	ALLHOSTSLIVE=1
+       else
+       	ALLHOSTSLIVE=0
+       	break
+       fi
+    done
+ done
+ echo +++All instances operational
 
 
 sudo ansible-playbook --limit=$COLOR -i /etc/ansible/hosts playbooks/homegrown/testco_init.yaml
